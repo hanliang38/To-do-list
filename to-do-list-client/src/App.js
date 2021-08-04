fc6c2a4adb5b2e681d0efc5f2cf3b8f0f190b83e
root <root@DESKTOP-IS6KDI5.localdomain>
2021-08-05 00:29:34 +0900

    1. client react setup

---
+++ to-do-list-client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="to-do-app">
+      <h1>To-Do-List</h1>
+    </div>
+  );
+}
+
+export default App;
