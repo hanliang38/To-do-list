d1b9de89bf35a55498708682a1f650ebc9fc4805
root <root@DESKTOP-IS6KDI5.localdomain>
2021-08-06 02:56:54 +0900

    1-3 add todos & remove todos

--- to-do-list-client/src/components/Todo.js
+++ to-do-list-client/src/components/Todo.js
@@ -1,11 +1,34 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import TodoForm from './TodoForm'
+import { TiEdit } from 'react-icons/ti'
+import { BiTrash } from 'react-icons/bi'
 
-function Todo() {
-    return (
-        <div>
-            
+function Todo({ todos, completeTodo, removeTodo }) {
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ''
+    })
+
+    return todos.map((todo, index) => (
+        <div
+            className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+            key={index}
+        >
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}
+            </div>
+            <div className='icons'>
+                <TiEdit
+                    className='edit-icon'
+                    onClick={() => setEdit({ id: todo.id, value: todo.text })}
+                />
+                <BiTrash
+                    className='delete-icon'
+                    onClick={() => removeTodo(todo.id)}
+                />
+            </div>
         </div>
-    )
+    ))
 }
 
 export default Todo
--- to-do-list-client/src/components/TodoList.js
+++ to-do-list-client/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo'
 
 function TodoList() {
     const [todos, setTodos] = useState([])
@@ -16,11 +17,30 @@ function TodoList() {
         // console.log(todo, ...todos)
     }
 
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr);
+    }
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        })
+        setTodos(updatedTodos);
+    }
+
     return (
         <div>
             <h1>To-Do-List</h1>
             <h3>오늘의 계획은 무엇인가요?</h3>
             <TodoForm onSubmit={addTodo} />
+            <Todo
+                todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}
+            />
         </div>
     )
 }
