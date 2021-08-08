13aaec16023379e1d75f5898c604678cd23f30c5
root <root@DESKTOP-IS6KDI5.localdomain>
2021-08-09 01:10:33 +0900

    1-5 client css

--- to-do-list-client/src/components/Todo.js
+++ to-do-list-client/src/components/Todo.js
@@ -22,26 +22,37 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     }
 
     return todos.map((todo, index) => (
-        <div
-            className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
-            key={index}
-        >
-            <div className='date'>
-            </div>
+        <>
+            <div
+                className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+                key={index}
+            >
             <div key={todo.id} onClick={() => completeTodo(todo.id)}>
                 {todo.text}
+                </div>
+
+                <div className='arr1'>
+                    <div className='ref'>ref</div>
+                </div>
+                <div className='date_icon'>
+                    <div className='arr2'>
+                        <div className='createdAt'>작성일: </div>
+                        <div className='updatedAt'>최근 수정일: </div>
+                    </div>
+                
+                    <div className='icons'>
+                        <TiEdit
+                        className='edit-icon'
+                        onClick={() => setEdit({ id: todo.id, value: todo.text })}
+                        />
+                        <BiTrash
+                        className='delete-icon'
+                        onClick={() => removeTodo(todo.id)}
+                        />
+                    </div>
+                </div>
             </div>
-            <div className='icons'>
-                <TiEdit
-                    className='edit-icon'
-                    onClick={() => setEdit({ id: todo.id, value: todo.text })}
-                />
-                <BiTrash
-                    className='delete-icon'
-                    onClick={() => removeTodo(todo.id)}
-                />
-            </div>
-        </div>
+        </>
     ))
 }
 
--- to-do-list-client/src/components/TodoList.js
+++ to-do-list-client/src/components/TodoList.js
@@ -5,7 +5,7 @@ import Todo from './Todo'
 function TodoList() {
     const [todos, setTodos] = useState([])
 
-    const addTodo = todo => {
+    const addTodo = todo => { // 정규식
         if (!todo.text || /^\s*$/.test(todo.text)) {
             return;
         }
